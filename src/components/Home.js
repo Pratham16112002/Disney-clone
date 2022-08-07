c81ac08865e52cdb4d4f82b1afb6d046b4672045
Prathm Dhiman <prathamdhiman@rediffmail.com>
2022-08-07 17:44:45 +0530

    still working bruh

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useEffect }from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Recommends from './Recommends'
@@ -6,8 +6,49 @@ import Viewer from './Viewer'
 import NewDisney from './NewDisney'
 import Orignals from './Orignals'
 import Trending from './Trending'
+import { useDispatch , useSelector } from 'react-redux'
+import db from '../firebase' 
+import { setMovies } from '../features/movie/movieSlice'
+import { selectUserName } from '../features/user/userSlice'
 
-function Home() {
+function Home(props) {
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = []; 
+
+  useEffect(() => {
+    console.log("Hello");
+    db.collection('movies').onSnapshot((snapshot) => {
+      snapshot.docs.map((doc) => {
+        console.log(recommends);
+        switch(doc.data().type) {
+          case "recommend":
+            recommends = [ ...recommends, { id: doc.id, ...doc.data()}];
+            break;
+          case "new" : 
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data()}];
+            break;
+          case "original":
+            originals = [ ...originals, { id: doc.id, ...doc.data() } ];
+            break;
+          case "trending":
+            trending = [ ...trending, { id: doc.id, ...doc.data() } ];
+            break;
+        }
+      })
+      dispatch(
+        setMovies({
+          recommend : recommends,
+          newDisneys : newDisneys,
+          original : originals,
+          trending : trending,
+        })
+      )
+    })
+  }, [userName]);
   return (
     <Container>
         <ImgSlider/>
